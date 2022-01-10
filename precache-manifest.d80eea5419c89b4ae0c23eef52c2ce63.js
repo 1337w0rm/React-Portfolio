a5b2c229030398c28caa218d2adb3293a729713f
Aditya Singh <baddrink.887@gmail.com>
2022-01-10 22:59:09 +0530

    Updates

---
+++ precache-manifest.d80eea5419c89b4ae0c23eef52c2ce63.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aefdb24ea18aab06f5a2e68ab07110e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "753ffe9f304592125da7",
+    "url": "/portfolio/static/css/2.458d69fc.chunk.css"
+  },
+  {
+    "revision": "753ffe9f304592125da7",
+    "url": "/portfolio/static/js/2.805d36c5.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/portfolio/static/js/2.805d36c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec68837785983040dd65",
+    "url": "/portfolio/static/js/main.829a1c1c.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "a54a89dc3469bc5505aef6b09e1b7bf6",
+    "url": "/portfolio/static/media/logo.a54a89dc.jpg"
+  },
+  {
+    "revision": "0604f0995f408faf92e120e2ea357db7",
+    "url": "/portfolio/static/media/pdf.0604f099.png"
+  },
+  {
+    "revision": "9eb4a78949b3b95669c8f4a2a17e47d8",
+    "url": "/portfolio/static/media/reverse.9eb4a789.png"
+  },
+  {
+    "revision": "24bff92e08268c5cbef6200fed6b5da6",
+    "url": "/portfolio/static/media/webpage.24bff92e.png"
+  }
+]);
